b8335b1af581d29c9d96a290f3498203f61271d4
tee2490 <44152787+tee2490@users.noreply.github.com>
2026-06-22 09:43:14 +0700

    Section02 Load Data from Map format

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ticket_app/base/res/app_styles.dart';
 import 'package:ticket_app/base/res/media.dart';
+import 'package:ticket_app/base/utils/all_json.dart';
 import 'package:ticket_app/base/widgets/app_double_text.dart';
 import 'package:ticket_app/base/widgets/ticket_view.dart';
 
@@ -69,7 +70,15 @@ class HomeScreen extends StatelessWidget {
                   bigText: 'Upcoming Flights',
                   smallText: 'View all',
                 ),
-                 TicketView(),
+                const SizedBox(height: 20),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: ticketList
+                        .map((singleTicket) => TicketView(ticket: singleTicket))
+                        .toList(),
+                  ),
+                ),
               ],
             ),
           ),
