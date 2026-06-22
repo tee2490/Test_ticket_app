ca38a927cb5899267ca97c34d8e6de2038b5fe5d
tee2490 <44152787+tee2490@users.noreply.github.com>
2026-06-22 11:11:37 +0700

    Section03 Dynamic navigation with callback

--- lib/base/widgets/app_double_text.dart
+++ lib/base/widgets/app_double_text.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:ticket_app/base/res/app_styles.dart';
-import 'package:ticket_app/screens/home/widgets/all_tickets.dart';
 
 class AppDoubleText extends StatelessWidget {
   const AppDoubleText({
     super.key,
     required this.bigText,
     required this.smallText,
+    required this.func,
   });
   final String bigText;
   final String smallText;
+  final VoidCallback func;
 
   @override
   Widget build(BuildContext context) {
@@ -18,14 +19,7 @@ class AppDoubleText extends StatelessWidget {
       children: [
         Text(bigText, style: AppStyles.headLineStyle2),
         InkWell(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (BuildContext context) => const AllTickets(),
-              ),
-            );
-          },
+          onTap: func,
           child: Text(
             smallText,
             style: AppStyles.textStyle.copyWith(color: AppStyles.primaryColor),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ticket_app/base/bottom_nav.dart';
+import 'package:ticket_app/base/utils/app_routes.dart';
+import 'package:ticket_app/screens/home/widgets/all_tickets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,8 +16,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(),
-      home: BottomNavBar(),
+      routes: {
+        AppRoutes.homePage: (context) => const BottomNavBar(),
+        AppRoutes.allTickets: (context) => const AllTickets(),
+      },
     );
   }
 }
-
