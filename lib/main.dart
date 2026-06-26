efe7f44cf26fbfee143e7212311574cd45990dac
tee2490 <44152787+tee2490@users.noreply.github.com>
2026-06-26 14:37:09 +0700

    Section06 Work on the hotel routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ticket_app/base/bottom_nav.dart';
 import 'package:ticket_app/base/utils/app_routes.dart';
+import 'package:ticket_app/screens/home/widgets/all_hotels.dart';
 import 'package:ticket_app/screens/home/widgets/all_tickets.dart';
 import 'package:ticket_app/screens/ticket/ticket_screen.dart';
 
@@ -21,6 +22,7 @@ class MyApp extends StatelessWidget {
         AppRoutes.homePage: (context) => const BottomNavBar(),
         AppRoutes.allTickets: (context) => const AllTickets(),
         AppRoutes.ticketScreen: (context) => const TicketScreen(),
+        AppRoutes.allHotels:(context)=> const AllHotels()
       },
     );
   }
