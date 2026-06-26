81d76114bea4ce72a07a5382e732d0bc3c696ab0
tee2490 <44152787+tee2490@users.noreply.github.com>
2026-06-26 10:17:52 +0700

    Section05 TicketScreen and tabs

--- lib/base/bottom_nav.dart
+++ lib/base/bottom_nav.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:ticket_app/screens/home/home_screen.dart';
 import 'package:ticket_app/screens/search/search_screen.dart';
+import 'package:ticket_app/screens/ticket/ticket_screen.dart';
 
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
@@ -15,7 +16,7 @@ class _BottomNavBarState extends State<BottomNavBar> {
   final appScreens = [
     const HomeScreen(),
     const SearchScreen(),
-    const Center(child: Text("Tickets")),
+    const TicketScreen(),
     const Center(child: Text("Profile")),
   ];
 
