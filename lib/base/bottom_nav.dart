5249b60ebbbf7e1c2bec1f9cc495a8fd3500320c
tee2490 <44152787+tee2490@users.noreply.github.com>
2026-06-27 13:24:06 +0700

    Section08 State managemetn BLOC & show BottomNavBar

--- lib/base/bottom_nav.dart
+++ lib/base/bottom_nav.dart
@@ -1,18 +1,17 @@
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:fluentui_system_icons/fluentui_system_icons.dart';
-import 'package:ticket_app/controller/bottom_nav_controller.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ticket_app/bloc/bottom_nav_bloc.dart';
+import 'package:ticket_app/bloc/bottom_nav_events.dart';
+import 'package:ticket_app/bloc/bottom_nav_states.dart';
 import 'package:ticket_app/screens/home/home_screen.dart';
 import 'package:ticket_app/screens/profile/profile.dart';
 import 'package:ticket_app/screens/search/search_screen.dart';
 import 'package:ticket_app/screens/ticket/ticket_screen.dart';
-import 'package:get/get.dart';
 
 class BottomNavBar extends StatelessWidget {
   BottomNavBar({super.key});
-
-  //dependency injection
-  final BottomNavController controller = Get.put(BottomNavController());
 
   //list is iterated using index
 
@@ -25,40 +24,50 @@ class BottomNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print("my tab val ${controller.selectedIndex.value}");
-    return Obx(() {
-      return Scaffold(
-        body: appScreens[controller.selectedIndex.value],
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: controller.selectedIndex.value,
-          onTap: controller.onItemTapped,
-          selectedItemColor: Colors.blueGrey,
-          unselectedItemColor: const Color(0xFF526400),
-          showSelectedLabels: false,
-          items: const [
-            BottomNavigationBarItem(
+    return BlocBuilder<BottomNavBloc, BottomNavState>(
+      builder: (context, state) {
+        print("my state is $state");
+
+        if (state is BottomNavBarSelected) {
+          return Scaffold(
+            body: appScreens[state.selectedIndex],
+            bottomNavigationBar: BottomNavigationBar(
+              currentIndex: state.selectedIndex,
+              onTap: (int index) {
+                context.read<BottomNavBloc>().add(OnItemTapped(index));
+              },
+              //onTap: ref.watch(bottomNavBarNotifierProvider.notifier).onItemTapped,
+              selectedItemColor: Colors.blueGrey,
+              unselectedItemColor: const Color(0xFF526400),
+              showSelectedLabels: false,
+              items: const [
+                BottomNavigationBarItem(
               icon: Icon(FluentIcons.home_24_regular),
               activeIcon: Icon(FluentIcons.home_24_filled),
-              label: "Home",
-            ),
-            BottomNavigationBarItem(
+                  label: "Home",
+                ),
+                BottomNavigationBarItem(
               icon: Icon(FluentIcons.search_24_regular),
               activeIcon: Icon(FluentIcons.search_24_filled),
-              label: "Search",
-            ),
-            BottomNavigationBarItem(
+                  label: "Search",
+                ),
+                BottomNavigationBarItem(
               icon: Icon(FluentIcons.ticket_diagonal_24_regular),
               activeIcon: Icon(FluentIcons.ticket_diagonal_24_filled),
-              label: "Tickets",
-            ),
-            BottomNavigationBarItem(
+                  label: "Tickets",
+                ),
+                BottomNavigationBarItem(
               icon: Icon(FluentIcons.person_24_regular),
               activeIcon: Icon(FluentIcons.person_24_filled),
-              label: "Profile",
+                  label: "Profile",
+                ),
+              ],
             ),
-          ],
-        ),
-      );
-    });
+          );
+        } else {
+          return Container();
+        }
+      },
+    );
   }
 }
